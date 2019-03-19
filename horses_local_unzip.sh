95379f8830cacb0d50888a01be9e49b56373ffc9
John Harrison <john.harrison@edgeware.tv>
2019-03-19 13:09:56 +0100

    script to unzip horses_local

---
+++ horses_local_unzip.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+tar -xzf horses_local/horse_local_2015_0.tar.gz
+tar -xzf horses_local/horse_local_2014_1.tar.gz
+tar -xzf horses_local/horse_local_2014_0.tar.gz
+tar -xzf horses_local/horse_local_2013_1.tar.gz
+tar -xzf horses_local/horse_local_2013_0.tar.gz
+tar -xzf horses_local/horse_local_2012_1.tar.gz
+tar -xzf horses_local/horse_local_2012_0.tar.gz
